db7805374ccb258ec4031c0db37c6dfba414022f
WilliamAraujo <w67543210@gmail.com>
2025-10-10 15:24:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h3>seu site aqui</h3>
+    </div>
+  );
+}
+
+export default App;
